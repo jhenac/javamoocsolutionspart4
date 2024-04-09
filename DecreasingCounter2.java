ee9257da6ccc4dd2916147d51f00a7d7e31cbc05
JC <98466796+jhenac@users.noreply.github.com>
2024-04-09 17:03:05 +0800

    Create DecreasingCounter2.java

---
+++ DecreasingCounter2.java
@@ -0,0 +1,45 @@
+// DecreasingCounter.java
+
+public class DecreasingCounter {
+
+    private int value;  // an object variable for storing the value of the counter
+
+    public DecreasingCounter(int initialValue) {
+        this.value = initialValue;
+    }
+
+    public void printValue() {
+        // Do not change this code!
+        System.out.println("value: " + this.value);
+    }
+
+    public void decrement() {
+        // write the method implementation here
+        // the aim is to decrement the value of the counter by one
+        if(this.value > 0) {
+            this.value = this.value - 1;
+        }
+    }
+
+    // the other methods go here
+}
+// -------------------------------------------------------------------
+// MainProgram.java
+
+public class MainProgram {
+
+    public static void main(String[] args) {
+        DecreasingCounter counter = new DecreasingCounter(2);
+
+        counter.printValue();
+        
+        counter.decrement();
+        counter.printValue();
+        
+        counter.decrement();
+        counter.printValue();
+        
+        counter.decrement();
+        counter.printValue();
+    }
+}
