c10890afe67400d422efe91e8c22705658f08b0f
JC <98466796+jhenac@users.noreply.github.com>
2024-04-09 15:57:19 +0800

    Create Whistle.java

---
+++ Whistle.java
@@ -0,0 +1,29 @@
+// Whistle.java
+public class Whistle {
+    private String sound;
+    
+    public Whistle(String whistleSound) {
+        this.sound = whistleSound;
+    }
+    
+    public void sound() {
+        System.out.println(sound);
+    }
+}
+
+// ------------------------------------------------
+// Main.java
+public class Main {
+
+    public static void main(String[] args) {
+        // This is just an empty main method you can use to test
+        // the Whistle class. Try for example:
+
+        Whistle duckWhistle = new Whistle("Kvaak");
+        Whistle roosterWhistle = new Whistle("Peef");
+
+        duckWhistle.sound();
+        roosterWhistle.sound();
+        duckWhistle.sound();
+    }
+}
