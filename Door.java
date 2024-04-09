a1d242e7268bd77f1f089a1512b1cf94a7b11bf7
JC <98466796+jhenac@users.noreply.github.com>
2024-04-09 16:10:44 +0800

    Create Door.java

---
+++ Door.java
@@ -0,0 +1,27 @@
+// Door.java
+public class Door {
+    
+    public Door(){
+        
+    }
+    
+    public void knock(){
+        System.out.println("Who's there?");
+    }
+}
+
+// ------------------------------------------
+
+// Main.java
+public class Main {
+
+    public static void main(String[] args) {
+        // This is an empty main method
+        // You can use it to test your Door class. Try the following:
+
+        Door alexander = new Door();
+
+        alexander.knock();
+        alexander.knock();
+    }
+}
